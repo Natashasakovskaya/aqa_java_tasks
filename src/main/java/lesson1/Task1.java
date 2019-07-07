94427e7b49db8be0a2a6d8af1024b95646635416
Naty <nataliiasakovska@hfhf.cbb>
2019-07-07 19:16:06 +0300

    done Lesson1, Task 1

--- src/main/java/lesson1/Task1.java
+++ src/main/java/lesson1/Task1.java
@@ -5,10 +5,50 @@ public class Task1 {
     Написать функцию, которая вычисляет минимум из трёх чисел.
     */
     public static void main(String[] args) {
-        //System.out.println(min(5,2,3));
+        System.out.println(
+                min(5,2,3)
+        );
+        System.out.println(
+                min(5,22,3)
+        );
+        System.out.println(
+                min(0,2,0)
+        );
+
+        System.out.println(
+                min1(5,2,3)
+        );
+        System.out.println(
+                min1(5,22,3)
+        );
+        System.out.println(
+                min1(0,2,0)
+        );
     }
 
     public static int min(int a, int b, int c) {
-        return 0;
+
+        if (a < b && a < c) {
+            return a;
+        } else if (b < a && b < c) {
+            return b;
+        } else {
+            return c;
+        }
     }
+    public static int min1(int a, int b, int c) {
+
+        int min = c;
+
+        if (min > b) {
+            min = b;
+        }
+
+        if (min > a) {
+            min = a;
+        }
+
+        return min;
+    }
+
 }
